0610284463bfda2a60b4b8a9e5650ddec9cf3a0b
Rafael Martins de Castro <42079080+Far3llProject@users.noreply.github.com>
2021-05-15 22:28:43 -0300

    vueTag id

--- src/boot/analytcs.js
+++ src/boot/analytcs.js
@@ -2,6 +2,6 @@ import VueGtag from 'vue-gtag'
 
 export default async ({ Vue, router }) => {
   Vue.use(VueGtag, {
-    config: { id: 'UA-154707501-1' }
+    config: { id: 'G-VJ4P0XP56Q' }
   }, router)
 }
